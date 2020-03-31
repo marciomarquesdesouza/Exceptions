9228f9f23d4099cbab4399ef3814a8ac51c3267c
Marcio Marques de Souza <marciomarques.contato@hotmail.com>
2020-03-31 00:05:26 -0300

    Validar funcionamento entre Eclipse e Git

--- exceptions/src/application/Program.java
+++ exceptions/src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Opa!");
+		System.out.println("Teste para analisar funcionamento entre Eclipse e Git");
 	}
 
 }
